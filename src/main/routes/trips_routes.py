bbe0132fb483939ef75edbe8dcf270f56f508e6e
gravena74 <gravena74jv@gmail.com>
2024-07-11 20:55:31 -0300

    finishing the class number 02

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -1,7 +1,39 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 trips_routes_bp = Blueprint("trip_routes", __name__)
 
+# Importação de Controllers
+
+from src.controllers.trip_creator import TripCreator
+from src.controllers.trip_finder import TripFinder
+
+# Importação de Repositorios
+
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+
+# Importando o gerente de conexões
+
+from src.models.settings.db_connection_handler import db_connection_handler
+
 @trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
-    return jsonify({"ola": "mundo"}), 200
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    emails_repository = EmailsToInviteRepository(conn)
+    controller = TripCreator(trips_repository, emails_repository)
+
+    response = controller.create(request.json)
+
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trips_routes_bp.route("/trips/<tripId>", methods=["GET"])
+def find_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripFinder(trips_repository)
+
+    response = controller.find_trip_details(tripId)
+
+    return jsonify(response["body"]), response["status_code"]
